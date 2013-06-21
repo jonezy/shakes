63502ffc6ebf4e084244a934333a95afc4fcb633
Chris Jones <chris@jonezy.org>
2013-06-21 14:29:05 -0400

    working through things
    Signed-off-by: Chris Jones <chris@jonezy.org>

--- test/shakes.js
+++ test/shakes.js
@@ -1,6 +1,15 @@
 var assert = require('assert');
+var mocha = require('mocha');
 var Shakes = require('../lib/shakes');
 
+var validOpts = {
+  'client_id': 'xxxxxxxxxx',
+  'client_secret': 'xxxxxxxxxxxxxx'
+};
+
+var invalidOptions = {
+  'test':'1234'
+};
 
 describe("shakes with empty constructor", function() {
   it("should throw an error", function() {
@@ -9,12 +18,75 @@ describe("shakes with empty constructor", function() {
 });
 
 describe("shakes with non-empty constructor", function() {
-  describe("with valid options", function() {
-
-  });
-
   describe("with in-valid options", function() {
-
+    it('should throw an error', function() {
+      assert.throws(function() { var moves = new Shakes(invalidOptions);}, Error);
+    });
+  });
+  describe('with missing option', function() {
+    describe('client_id', function() {
+      it('should throw an error', function() {
+        assert.throws(function() {
+          var moves = new Shakes({'client_secret':'1234','redirect_uri':'/'});
+        }, Error);
+      });
+    });
+    describe('client_secret', function() {
+      it('should throw an error', function() {
+        assert.throws(function() {
+          var moves = new Shakes({'client_id':'1234','redirect_uri':'/'});
+        }, Error);
+      });
+    });
   });
 
+  describe("with valid options", function() {
+    var moves;
+    beforeEach(function() {
+      moves = new Shakes(validOpts);
+    });
+
+    it('should return an object', function() {
+      assert(moves, typeof(object), 'expected an object');
+    });
+
+    it('should have a client_id property', function() {
+      assert(moves.client_id, validOpts.client_id, 'client_id did not match');
+    });
+
+    it('should have a client_secret property', function() {
+      assert(moves.client_secret, validOpts.client_secret, 'client_secret did not match');
+    });
+  });
+});
+
+describe("shakes.authorize()", function() {
+
+  describe("with valid options", function() {
+    var moves;
+    beforeEach(function() {
+      moves = new Shakes(validOpts);
+    });
+
+    it('should return a string', function() {
+      assert(moves.authorize({'redirect_uri':'test', 'scope':'locations'}), typeof(string), 'expected an object');
+    });
+  });
+
+  describe('with missing option', function() {
+    describe('redirect_uri', function() {
+      it('should throw an error', function() {
+        assert.throws(function() {
+          moves.authorize();
+        }, Error);
+      });
+    });
+    describe('scope', function() {
+      it('should throw an error', function() {
+        assert.throws(function() {
+          moves.authorize({'redirect_uri':'test'});
+        }, Error);
+      });
+    });
+  });
 });
