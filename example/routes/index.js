4c33dcdba6e09e998a27de6421b76a250daea728
Tyler Sloan <tyler.g.sloan@gmail.com>
2014-06-18 13:30:00 -0400

    Change your IP to localhost
    The app wasn't successfully saving my token until I saw this and changed it locally–after that everything works as expected.

--- example/routes/index.js
+++ example/routes/index.js
@@ -21,7 +21,7 @@ exports.index = function(req, res){
 
   var moves = new Shakes(shakesOpts);
   var auth_url = moves.authorize({'scope':'activity location'});
-  var mobile_auth_url = moves.authorize({'scope':'activity location', 'urlScheme':'mobile', 'redirect_uri': 'http://192.168.1.129:3000/auth/token'});
+  var mobile_auth_url = moves.authorize({'scope':'activity location', 'urlScheme':'mobile', 'redirect_uri': 'http://localhost:3000/auth/token'});
 
   res.render('index', { title: 'Shakes Example', auth_url: auth_url, mobile_auth_url: mobile_auth_url, token: t, has_token:hasToken });
 };
